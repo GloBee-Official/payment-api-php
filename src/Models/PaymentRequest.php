4221649f9db280934cd68fc225616cb2b68d5f24
Bernhard Breytenbach <bernhard@globee.com>
2018-04-23 15:05:19 +0200

    Replace redundant methods with magic methods

--- src/Models/PaymentRequest.php
+++ src/Models/PaymentRequest.php
@@ -76,14 +76,6 @@ class PaymentRequest extends Model
     }
 
     /**
-     * @return mixed
-     */
-    public function getTotal()
-    {
-        return $this->total;
-    }
-
-    /**
      * @param float $total
      *
      * @throws \GloBee\PaymentApi\Exceptions\Validation\InvalidArgumentException
@@ -93,14 +85,6 @@ class PaymentRequest extends Model
     {
         $this->validateNumberAboveMinimum('total', $total, 0);
         $this->total = $total;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCurrency()
-    {
-        return $this->currency;
     }
 
     /**
@@ -116,62 +100,6 @@ class PaymentRequest extends Model
     }
 
     /**
-     * @return mixed
-     */
-    public function getCustomPaymentId()
-    {
-        return $this->customPaymentId;
-    }
-
-    /**
-     * @param mixed $customPaymentId
-     */
-    public function setCustomPaymentId($customPaymentId)
-    {
-        $this->customPaymentId = $customPaymentId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCallbackData()
-    {
-        return $this->callbackData;
-    }
-
-    /**
-     * @param mixed $callbackData
-     */
-    public function setCallbackData($callbackData)
-    {
-        $this->callbackData = $callbackData;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCustomerName()
-    {
-        return $this->customerName;
-    }
-
-    /**
-     * @param mixed $customerName
-     */
-    public function setCustomerName($customerName)
-    {
-        $this->customerName = $customerName;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCustomerEmail()
-    {
-        return $this->customerEmail;
-    }
-
-    /**
      * @param mixed $customerEmail
      *
      * @throws \GloBee\PaymentApi\Exceptions\Validation\InvalidArgumentException
@@ -181,14 +109,6 @@ class PaymentRequest extends Model
     {
         $this->validateEmail('customer.email', $customerEmail);
         $this->customerEmail = $customerEmail;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSuccessUrl()
-    {
-        return $this->successUrl;
     }
 
     /**
@@ -204,28 +124,12 @@ class PaymentRequest extends Model
     }
 
     /**
-     * @return mixed
-     */
-    public function getCancelUrl()
-    {
-        return $this->cancelUrl;
-    }
-
-    /**
      * @param mixed $cancelUrl
      */
     public function setCancelUrl($cancelUrl)
     {
         $this->validateUrl('cancel_url', $cancelUrl);
         $this->cancelUrl = $cancelUrl;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getIpnUrl()
-    {
-        return $this->ipnUrl;
     }
 
     /**
@@ -238,28 +142,12 @@ class PaymentRequest extends Model
     }
 
     /**
-     * @return mixed
-     */
-    public function getNotificationEmail()
-    {
-        return $this->notificationEmail;
-    }
-
-    /**
      * @param mixed $notificationEmail
      */
     public function setNotificationEmail($notificationEmail)
     {
         $this->validateEmail('notification_email', $notificationEmail);
         $this->notificationEmail = $notificationEmail;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getConfirmationSpeed()
-    {
-        return $this->confirmationSpeed;
     }
 
     /**
@@ -271,46 +159,6 @@ class PaymentRequest extends Model
     {
         $this->validateOptions('confirmation_speed', $confirmationSpeed, ['low', 'medium', 'high']);
         $this->confirmationSpeed = $confirmationSpeed;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getRedirectUrl()
-    {
-        return $this->redirectUrl;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getExpiresAt()
-    {
-        return $this->expiresAt;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
     }
 
     public function toArray()
--- src/Models/PropertyTrait.php
+++ src/Models/PropertyTrait.php
@@ -24,11 +24,27 @@ trait PropertyTrait
         }
     }
 
+    public function __call($name, $arguments)
+    {
+        $mutator = substr($name, 0, 3);
+        if ($mutator === 'get') {
+            return $this->__get(lcfirst(substr($name, 3)));
+        }
+
+        if ($mutator === 'set') {
+            return $this->__set(lcfirst(substr($name, 3)), $arguments[0]);
+        }
+    }
+
     public function __get($name)
     {
         $methodName = 'get'.$this->strToStudlyCase($name);
         if (method_exists($this, $methodName)) {
             return $this->{$methodName}();
+        }
+
+        if (property_exists($this, $name)) {
+            return $this->{$name};
         }
     }
 
@@ -37,6 +53,12 @@ trait PropertyTrait
         $methodName = 'set'.$this->strToStudlyCase($name);
         if (method_exists($this, $methodName)) {
             $this->{$methodName}($value);
+
+            return;
+        }
+
+        if (property_exists($this, $name)) {
+            $this->{$name} = $value;
 
             return;
         }
